8520e03143da67ee16d7f42936f9c09161321179
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2026-01-18 12:37:42 +0530

    chore: remove sensitive information from .env and update .gitignore to include it

--- config/mongo.db.js
+++ config/mongo.db.js
@@ -1,9 +1,10 @@
-const mongoose = require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 async function connectDB() {
     try {
         console.log("Connecting DB...");
-        await mongoose.connect("mongodb://root:qwerty@localhost:27017/notesDB?authSource=admin");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected DB.");
     } catch (e) {
         console.log("failed to connect DB!!", e);
