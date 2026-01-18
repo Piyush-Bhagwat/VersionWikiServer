f0e54b138dae1a04b4412ea61ab270493768572a
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2026-01-18 21:51:47 +0530

    trying manual headers

--- server.js
+++ server.js
@@ -15,11 +15,29 @@ const __dirname = dirname(__filename);
 
 const app = express();
 logger.useDefaults();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization",
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "false");
+
+    // Handle preflight
+    if (req.method === "OPTIONS") {
+        return res.status(204).end();
+    }
+
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
 app.use(responseHandler);
 
 let dbConnected = false;
