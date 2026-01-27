ba837ea3d44b4ffad166ebf4722de30ceccc9325
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2026-01-27 21:48:18 +0530

    refactor: replace custom CORS headers with cors middleware

--- server.js
+++ server.js
@@ -15,25 +15,12 @@ const __dirname = dirname(__filename);
 
 const app = express();
 logger.useDefaults();
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization",
-    );
-    res.setHeader("Access-Control-Allow-Credentials", "false");
-
-    // Handle preflight
-    if (req.method === "OPTIONS") {
-        return res.status(204).end();
-    }
-
-    next();
-});
+app.use(
+    cors({
+        origin: "https://version-wiki-client-git-pre-prod-abnormal.vercel.app",
+        credentials: true,
+    }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
